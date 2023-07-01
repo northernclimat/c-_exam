0decc5e8d610a2a0f0a1a5ddbf1c39e91a82a0f8
Denis Kozin(home) <70297257+Akishev23@users.noreply.github.com>
2023-07-01 05:53:30 +0300

    writing scheme

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿//? Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+//? Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] GetArray()
+{
+    Console.WriteLine("Please, input your array");
+
+    string inputtedString = Console.ReadLine();
+    
+    if (inputtedString.Length > 0) return inputtedString.Split(",");
+    else return null;
+}
+
+string GetNewArray(string[] arr)
+{
+    var newArray = new string[arr.Length];
+    int newArraySize = 0;
+    foreach(var str in arr)
+    {
+        if (str.Length < 4) 
+        {
+            newArray[newArraySize] = str;
+            newArraySize ++;
+        }
+    }
+
+    return string.Join(',', newArray);
+}
+
+string[] arr = GetArray();
+
+Console.WriteLine(string.Join(',', arr));
+
+Console.WriteLine($"Новый массив - {GetNewArray(arr)}");
+
+
